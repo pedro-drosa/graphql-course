ae7a6599948c784474276dbdcab36a37ad516ff1
pedro-drosa <pedro_drosa@hotmail.com>
2022-05-27 16:42:15 -0300

    feat(db): generate id dynamically

--- mutation/projeto/data/db.js
+++ mutation/projeto/data/db.js
@@ -1,20 +1,23 @@
+let initialId = 1;
+
+export const nextId = () => initialId++;
+
 export const users = [
   {
-    id: 1, name: 'Anakin Skywalker', email: 'anakin@skywalker.com', age: 26, profileId: 1, status: 'ACTIVE',
+    id: nextId(), name: 'Anakin Skywalker', email: 'anakin@skywalker.com', age: 26, profileId: 1, status: 'ACTIVE',
   },
   {
-    id: 2, name: 'Obi-Wan Kenobi', email: 'obi-Wan@Kenobi.com', age: 36, profileId: 2, status: 'INACTIVE',
+    id: nextId(), name: 'Obi-Wan Kenobi', email: 'obi-Wan@Kenobi.com', age: 36, profileId: 2, status: 'INACTIVE',
   },
   {
-    id: 3, name: 'Luke Skywalker', email: 'luke@skywalker.com', age: 19, profileId: 1, status: 'BLOCKED',
+    id: nextId(), name: 'Luke Skywalker', email: 'luke@skywalker.com', age: 19, profileId: 1, status: 'BLOCKED',
   },
   {
-    id: 4, name: 'Han Solo', email: 'han@solo.com', age: 19, profileId: 2, status: 'ACTIVE',
+    id: nextId(), name: 'Han Solo', email: 'han@solo.com', age: 19, profileId: 2, status: 'ACTIVE',
   },
-
 ];
 
 export const profiles = [
   { id: 1, name: 'common' },
-  { id: 2, name: 'admiministrator' },
+  { id: 2, name: 'administrator' },
 ];
