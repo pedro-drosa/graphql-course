0e337b5fa0ace82161b7f497246602515b4aa0a7
pedro-drosa <pedro_drosa@hotmail.com>
2022-05-28 12:15:12 -0300

    feat: delete profile by id

--- mutation/projeto/resolvers/Mutation/Profile.js
+++ mutation/projeto/resolvers/Mutation/Profile.js
@@ -1,8 +1,21 @@
 import { profiles } from '../../data/db';
 
+const findProfileIndex = (filter) => {
+  if (!filter) return -1;
+  const { id } = filter;
+  if (id) return profiles.findIndex((profile) => profile.id === id);
+  return -1;
+};
+
 export default {
   newProfile: (_, { data }) => {
     profiles.push({ id: profiles.length + 1, name: data.name });
     return profiles[profiles.length - 1];
+  },
+
+  deleteProfile: (_, { filter }) => {
+    const indexExists = findProfileIndex(filter);
+    const [deletedProfile] = profiles.splice(indexExists, 1);
+    return deletedProfile;
   },
 };
