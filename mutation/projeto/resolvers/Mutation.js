65e1e95d1306042a54812ef424b0845e436425bd
pedro-drosa <pedro_drosa@hotmail.com>
2022-05-27 22:34:30 -0300

    feat: update user by id

--- mutation/projeto/resolvers/Mutation.js
+++ mutation/projeto/resolvers/Mutation.js
@@ -4,7 +4,6 @@ export default {
   newUser: (_, args) => {
     const existingEmail = users.some(((user) => user.email === args.email));
     if (existingEmail) throw new Error('Email already registered');
-
     const newUser = {
       id: nextId(),
       ...args,
@@ -20,5 +19,16 @@ export default {
     if (indexExists < 0) return null;
     const [deletedUSer] = users.splice(indexExists, 1);
     return deletedUSer;
+  },
+
+  changeUser: (_, args) => {
+    const indexExists = users.findIndex((user) => user.id === args.id);
+    if (indexExists < 0) return null;
+    const updatedUser = {
+      ...users[indexExists],
+      ...args,
+    };
+    users.splice(indexExists, 1, updatedUser);
+    return updatedUser;
   },
 };
